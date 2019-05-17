6f79d7f86d0e107f632444f80024eb472978f97b
ken <redcool007@qq.com>
2019-05-17 23:12:16 +0800

    fixed Texture2DEx

--- UtilityLib/UnityScripts/Extentions/Texture2DEx.cs
+++ UtilityLib/UnityScripts/Extentions/Texture2DEx.cs
@@ -22,21 +22,23 @@ public static class Texture2DEx
         }
     }
 
-    public static void FillCircle(this Texture2D tex, Vector2Int center, Texture2D mask)
+    public static void FillCircle(this Texture2D tex, Vector2Int center, Texture2D stamp)
     {
-        var start = center - new Vector2Int(mask.width / 2, mask.height / 2);
-        var block = new Vector2Int(tex.width, tex.height) - start;
-        block = new Vector2Int(Mathf.Min(block.x, mask.width), Mathf.Min(block.y, mask.height));
+        var start = center - new Vector2Int(stamp.width / 2, stamp.height / 2);
+        var block = new Vector2Int(stamp.width, stamp.height);
+        var b1 = new BoundsInt(start.x,start.y,0,block.x,block.y,0);
+        var b2 = new BoundsInt(0, 0, 0, tex.width, tex.height,0);
+        b1.ClampToBounds(b2);
 
-        var maskColors = mask.GetPixels();
-        var colors = tex.GetPixels(start.x, start.y, block.x, block.y);
+        var stampColor = stamp.GetPixels();
+        var colors = tex.GetPixels(b1.x, b1.y, b1.size.x, b1.size.y);
 
         for (int i = 0; i < colors.Length; i++)
         {
             var c = colors[i];
-            var mc = maskColors[i];
+            var mc = stampColor[i];
             colors[i] = c * mc;
         }
-        tex.SetPixels(start.x, start.y, block.x, block.y, colors);
+        tex.SetPixels(b1.x, b1.y, b1.size.x, b1.size.y, colors);
     }
 }
