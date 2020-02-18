e4a99d7c7c6e9560a2669ce65976df59a83e6d9a
ken <redcool007@qq.com>
2020-02-18 12:20:57 +0800

    add DayTimeAnimation

---
+++ UtilityLib/UnityScripts/DayTimeAnimation/DayTimeAnimationDriver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 控制动画按时间(24h)进行播放
+/// </summary>
+public class DayTimeAnimationDriver : MonoBehaviour
+{
+    [Header("(游戏)一天是(现实中)多少秒?")]
+    [Min(1)]
+    public float secondsADay = 30;
+
+    float elapsedSecs;
+    [Header("Debug Info")]
+    [SerializeField]private float timeRate;
+    [SerializeField]float hour;
+
+    static List<DayTimeAnimationItem> animList = new List<DayTimeAnimationItem>();
+    public static void Add(DayTimeAnimationItem item) {
+        if (!animList.Contains(item))
+        {
+            animList.Add(item);
+        }
+    }
+    public static void Remove(DayTimeAnimationItem item)
+    {
+        if (animList.Contains(item))
+        {
+            animList.Remove(item);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimeRate();
+        hour = timeRate * 24;
+
+        UpdateAnimations();
+    }
+
+    private void UpdateAnimations()
+    {
+        foreach (var item in animList)
+        {
+            if(item.isActiveAndEnabled)
+                item.UpdateAnimation(timeRate);
+        }
+    }
+
+    void UpdateTimeRate()
+    {
+        elapsedSecs += Time.deltaTime;
+        elapsedSecs %= secondsADay;
+
+        timeRate = elapsedSecs / secondsADay;
+    }
+}
---
+++ UtilityLib/UnityScripts/DayTimeAnimation/DayTimeAnimationItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+/// <summary>
+/// 24h动画播放控制项
+/// </summary>
+public class DayTimeAnimationItem :MonoBehaviour
+{
+    [SerializeField]Animation anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+        enabled = anim && anim.clip;
+        if (anim)
+        {
+            anim.playAutomatically = false;
+            anim.Stop();
+        }
+    }
+
+    private void OnEnable()
+    {
+        DayTimeAnimationDriver.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        DayTimeAnimationDriver.Remove(this);
+    }
+
+    public void UpdateAnimation(float timeRate)
+    {
+        if (!anim || !anim.clip)
+            return;
+
+        anim.clip.SampleAnimation(gameObject,timeRate * anim.clip.length);
+    }
+}
