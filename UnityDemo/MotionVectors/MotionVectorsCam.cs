1ec02bce8c66e6f1ab62bc704d30c7203a4fb76f
ken <redcool007@qq.com>
2020-04-15 13:12:55 +0800

    add MotionVectors

---
+++ UnityDemo/MotionVectors/MotionVectorsCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionVectorsCam : MonoBehaviour
+{
+    public Shader motionVectorShader;
+    Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.MotionVectors;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!mat)
+        {
+            mat = new Material(motionVectorShader);
+        }
+
+        Graphics.Blit(source, destination, mat);
+    }
+}
