e2a075debd26c5595537107bca3368278d422ad8
ken <redcool007@qq.com>
2020-05-29 13:40:02 +0800

    add trigger

---
+++ UtilityLib/UnityScripts/Trigger/ColliderTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderTrigger : MonoBehaviour
+{
+    public UnityEvent OnTrigerEnterEvent;
+    public UnityEvent OnTriggerExitEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        OnTrigerEnterEvent.Invoke();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        OnTriggerExitEvent.Invoke();
+    }
+}
