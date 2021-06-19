c8ba2b5c5bc76adf5408b54ec7d9701f1989c4e6
ken <redcool007@qq.com>
2021-06-19 13:24:08 +0800

    update MeshIdCreator

--- Shader/PowerXXX/PowerMeshId/meshId/Scripts/MeshIdCreator.cs
+++ Shader/PowerXXX/PowerMeshId/meshId/Scripts/MeshIdCreator.cs
@@ -10,10 +10,10 @@ namespace PowerUtilities
     [CustomEditor(typeof(MeshIdCreator))]
     public class MeshIdCreatorEditor : Editor
     {
-        public void CombineMeshes(Mesh[] meshes, string outputDir, string meshIdName, float unitWidth)
+        public Mesh CombineMeshes(Mesh[] meshes, string outputDir, string meshIdName, float unitWidth)
         {
             if (meshes == null || meshes.Length == 0)
-                return;
+                return null;
 
             var vertexList = new List<Vector3>();
             var indexList = new List<int>();
@@ -58,10 +58,10 @@ namespace PowerUtilities
             mesh.RecalculateNormals();
 
             var path = $"{outputDir}/{meshIdName}.asset";
-            AssetDatabase.DeleteAsset(path);
-            AssetDatabase.CreateAsset(mesh, path);
-            AssetDatabase.Refresh();
+            return CreateAndLoadAsset<Mesh>(mesh, path);
         }
+
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
@@ -71,23 +71,103 @@ namespace PowerUtilities
             if (GUILayout.Button("Create"))
             {
                 if (string.IsNullOrEmpty(inst.outputDir))
-                    inst.outputDir = "Assets/";
+                    inst.outputDir = "Assets";
 
                 var meshes = inst.GetMeshes();
 
-                CombineMeshes(meshes, inst.outputDir, inst.meshIdName, inst.unitWidth);
+                var meshIdMesh = CombineMeshes(meshes, inst.outputDir, inst.meshIdName, inst.unitWidth);
+                CreatePrefabs(meshIdMesh, inst.meshGos,inst.outputDir, inst.unitWidth,inst.meshIdShader,inst.destroyGameObjects);
             }
+
+
+        }
+
+        Material SaveMeshIdMaterial(Shader meshIdShader,string path)
+        {
+            if (!meshIdShader)
+            {
+                meshIdShader = Shader.Find("Unlit/MeshId");
+            }
+
+            var mat = new Material(meshIdShader);
+            return CreateAndLoadAsset<Material>(mat, path);
+        }
+
+        void CreatePrefabs(Mesh meshIdMesh, GameObject[] originalGos,string outputDir, float unitWidth,Shader meshIdShader,bool destroyGameObjects)
+        {
+            var dir = CreateFolder(outputDir,"prefabs");
+
+            var mat = SaveMeshIdMaterial(meshIdShader, $"{outputDir}/meshId.mat");
+
+            for (int i = 0; i < originalGos.Length; i++)
+            {
+                var originalGo = originalGos[i];
+                var prefabName = originalGo.name;
+
+                GameObject meshIdGo = CreateMeshIdGo(meshIdMesh, unitWidth, mat, i, prefabName);
+
+                var path = $"{dir}/{prefabName}.prefab";
+                PrefabUtility.SaveAsPrefabAssetAndConnect(meshIdGo, path, InteractionMode.AutomatedAction);
+                if(destroyGameObjects)
+                    DestroyImmediate(meshIdGo);
+            }
+        }
+
+        private static GameObject CreateMeshIdGo(Mesh meshIdMesh, float unitWidth, Material mat, int meshId, string prefabName)
+        {
+            var go = new GameObject(prefabName);
+            var mf = go.AddComponent<MeshFilter>();
+            mf.sharedMesh = meshIdMesh;
+
+            var mr = go.AddComponent<MeshRenderer>();
+            mr.sharedMaterial = mat;
+
+            var blockSetter = go.AddComponent<MaterialPropertyBlockSetter>();
+            blockSetter.infos = new[] {
+                    new ShaderPropertyInfo{propName="_MeshId",floatPropValue = meshId},
+                    new ShaderPropertyInfo{propName="_OffsetX",floatPropValue = -meshId * unitWidth},
+                };
+            blockSetter.UpdateInstancedProperties();
+            return go;
+        }
+
+        public static string CreateFolder(string outputDir, string subFolderName)
+        {
+            var dir = $"{outputDir}/{subFolderName}";
+            if (AssetDatabase.IsValidFolder(dir))
+            {
+                AssetDatabase.DeleteAsset(dir);
+            }
+
+            var guid = AssetDatabase.CreateFolder(outputDir, subFolderName);
+            dir = AssetDatabase.GUIDToAssetPath(guid);
+            return dir;
+        }
+
+        public static T CreateAndLoadAsset<T>(Object asset, string path) where T : Object
+        {
+            AssetDatabase.DeleteAsset(path);
+            AssetDatabase.CreateAsset(asset, path);
+            AssetDatabase.Refresh();
+
+            return AssetDatabase.LoadAssetAtPath<T>(path);
         }
     }
 #endif
 
     public class MeshIdCreator : MonoBehaviour
     {
+        [Header("Output")]
+        public float unitWidth = 4;
+        public string outputDir = "Assets";
+        [Header("Mesh info")]
         //public Mesh[] meshes;
         public GameObject[] meshGos;
-        public float unitWidth = 4;
-        public string outputDir;
         public string meshIdName = "TestMeshId";
+
+        [Header("Prefab info")]
+        public Shader meshIdShader;
+        public bool destroyGameObjects = true;
 
         public Mesh[] GetMeshes()
         {
