c7f437efd23ec825689a0d14cc9e0231699ce97c
ken <redcool007@qq.com>
2021-07-08 12:36:15 +0800

    update Tex2DArrayCreator

--- Shader/Demo/2dArray/Tex2DArrayCreator.cs
+++ Shader/Demo/2dArray/Tex2DArrayCreator.cs
@@ -1,63 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿namespace PowerUtilities
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 #if UNITY_EDITOR
 
-using UnityEditor;
-[CustomEditor(typeof(Tex2DArrayCreator))]
-public class Tex2DArrayEditor : Editor
-{
-    Texture2DArray CreateTexArray(Texture2D[] texs,bool isLinear)
+    using UnityEditor;
+    [CustomEditor(typeof(Tex2DArrayCreator))]
+    public class Tex2DArrayEditor : Editor
     {
-        var tex = texs[0];
-        var texArr = new Texture2DArray(tex.width, tex.height, texs.Length, tex.format,tex.mipmapCount>0,false);
-
-        int index = 0;
-        foreach (var item in texs)
+        Texture2DArray CreateTexArray(Texture2D[] texs, bool isLinear)
         {
-            Graphics.CopyTexture(item, 0, texArr, index);
-            //for (int m = 0; m < item.mipmapCount; m++)
-            //{
-            //    Graphics.CopyTexture(item, 0, m, texArr,index, m);
-            //}
-            index++;
-        }
-        texArr.Apply(false,true);
-        return texArr;
-    }
-    /*
-    Texture2DArray CreateTexArray(Texture2D[] texs)
-    {
-        var texArr = new Texture2DArray(texs[0].width, texs[0].height, texs.Length, TextureFormat.RGB24, true);
+            var tex = texs[0];
+            var texArr = new Texture2DArray(tex.width, tex.height, texs.Length, tex.format, tex.mipmapCount > 0, false);
 
-        int index = 0;
-        foreach (var item in texs)
+            int index = 0;
+            foreach (var item in texs)
+            {
+                Graphics.CopyTexture(item, 0, texArr, index);
+                index++;
+            }
+            texArr.Apply(false, true);
+            return texArr;
+        }
+        /*
+        Texture2DArray CreateTexArray(Texture2D[] texs)
         {
-            texArr.SetPixels(item.GetPixels(), index++);
-        }
-        texArr.Apply();
-        return texArr;
-    }
-    */
+            var texArr = new Texture2DArray(texs[0].width, texs[0].height, texs.Length, TextureFormat.RGB24, true);
 
-    public override void OnInspectorGUI()
-    {
-        var inst = ((Tex2DArrayCreator)target);
-        base.OnInspectorGUI();
-        if (GUILayout.Button("Create"))
+            int index = 0;
+            foreach (var item in texs)
+            {
+                texArr.SetPixels(item.GetPixels(), index++);
+            }
+            texArr.Apply();
+            return texArr;
+        }
+        */
+
+        public override void OnInspectorGUI()
         {
+            base.OnInspectorGUI();
+            var inst = ((Tex2DArrayCreator)target);
+            if (GUILayout.Button("Create"))
+            {
+                var texArr = CreateTexArray(inst.texs, inst.isLinear);
 
-            var texArr = CreateTexArray(inst.texs, inst.isLinear);
-            AssetDatabase.DeleteAsset("Assets/Test/texArr.asset");
-            AssetDatabase.CreateAsset(texArr, "Assets/Test/texArr.asset");
+                var path = $"{inst.outputDir}/{inst.texArrName}";
+                AssetDatabase.DeleteAsset(path);
+                AssetDatabase.CreateAsset(texArr, path);
+                AssetDatabase.Refresh();
+            }
+
         }
-
     }
-}
 #endif
 
-public class Tex2DArrayCreator : MonoBehaviour
-{
-    public Texture2D[] texs;
-    public bool isLinear = true;
+    public class Tex2DArrayCreator : MonoBehaviour
+    {
+        public Texture2D[] texs;
+        public bool isLinear = true;
+        public string outputDir = "Assets/";
+        public string texArrName = "texArr.asset";
+    }
 }
