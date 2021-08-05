6e1f5b70c671ccee9282c9d3b9a4902a0286ea56
ken <redcool007@qq.com>
2021-08-05 23:09:55 +0800

    add inputasset

---
+++ UtilityLib/UnityScripts/InputAssets/PlayerInputSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInputSystem
+{
+    static PlayerInputAsset inputAsset;
+
+    public static PlayerInputAsset.PlayerControlActions InputMaster
+    {
+        get
+        {
+            if (inputAsset == null)
+            {
+                inputAsset = new PlayerInputAsset();
+                inputAsset.Enable();
+            }
+            return inputAsset.PlayerControl;
+        }
+    }
+
+    public static bool IsFire0Pressed()
+    {
+        return InputMaster.Fire0.ReadValue<float>() > 0;
+    }
+}
