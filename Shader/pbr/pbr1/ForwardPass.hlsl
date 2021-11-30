ce13dfda54f46a9a7db6ccc4e354580b40429dd0
ken <redcool007@qq.com>
2021-11-30 23:39:16 +0800

    add pbr1 demo

---
+++ Shader/pbr/pbr1/ForwardPass.hlsl
@@ -0,0 +1,141 @@
+#if !defined(FORWARD_PASS_HLSL)
+#define FORWARD_PASS_HLSL
+
+
+#define TRANSFORM_TEX(tex, name) ((tex.xy) * name##_ST.xy + name##_ST.zw)
+
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+#if !defined(USING_STEREO_MATRICES)
+float4x4 glstate_matrix_projection;
+float4x4 unity_MatrixV;
+float4x4 unity_MatrixInvV;
+float4x4 unity_MatrixInvP;
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixInvVP;
+float4 unity_StereoScaleOffset;
+int unity_StereoEyeIndex;
+#endif
+
+#define UNITY_MATRIX_M     unity_ObjectToWorld
+#define UNITY_MATRIX_I_M   unity_WorldToObject
+#define UNITY_MATRIX_V     unity_MatrixV
+#define UNITY_MATRIX_I_V   unity_MatrixInvV
+#define UNITY_MATRIX_P     (glstate_matrix_projection)
+#define UNITY_MATRIX_I_P   unity_MatrixInvP
+#define UNITY_MATRIX_VP    unity_MatrixVP
+#define UNITY_MATRIX_I_VP  unity_MatrixInvVP
+#define UNITY_MATRIX_MV    mul(UNITY_MATRIX_V, UNITY_MATRIX_M)
+#define UNITY_MATRIX_T_MV  transpose(UNITY_MATRIX_MV)
+#define UNITY_MATRIX_IT_MV transpose(mul(UNITY_MATRIX_I_M, UNITY_MATRIX_I_V))
+#define UNITY_MATRIX_MVP   mul(UNITY_MATRIX_VP, UNITY_MATRIX_M)
+
+float3 TransformObjectToWorld(float3 objectPos){
+    return mul(UNITY_MATRIX_M,float4(objectPos,1));
+}
+float4 TransformObjectToHClip(float3 objectPos){
+    return mul(UNITY_MATRIX_VP,mul(UNITY_MATRIX_M,float4(objectPos,1)));
+}
+
+float3 TransformObjectToWorldNormal(float3 normal){
+    return mul(float4(normal,1),UNITY_MATRIX_I_M);
+}
+
+
+
+
+float4 _MainLightPosition;
+half4 _MainLightColor;
+
+float3 _WorldSpaceCameraPos;
+
+
+float3 GetWorldSpaceViewDir(float3 worldPos){
+    return _WorldSpaceCameraPos - worldPos;
+}
+
+float3 GetWorldSpaceLightDir(float3 worldPos){
+    return _MainLightPosition.xyz - worldPos;
+}
+
+float4 unity_SHAr;
+float4 unity_SHAg;
+float4 unity_SHAb;
+float4 unity_SHBr;
+float4 unity_SHBg;
+float4 unity_SHBb;
+float4 unity_SHC;
+
+// Ref: "Efficient Evaluation of Irradiance Environment Maps" from ShaderX 2
+float3 SHEvalLinearL0L1(float3 N, float4 shAr, float4 shAg, float4 shAb)
+{
+    float4 vA = float4(N, 1.0);
+
+    float3 x1;
+    // Linear (L1) + constant (L0) polynomial terms
+    x1.r = dot(shAr, vA);
+    x1.g = dot(shAg, vA);
+    x1.b = dot(shAb, vA);
+
+    return x1;
+}
+
+float3 SHEvalLinearL2(float3 N, float4 shBr, float4 shBg, float4 shBb, float4 shC)
+{
+    float3 x2;
+    // 4 of the quadratic (L2) polynomials
+    float4 vB = N.xyzz * N.yzzx;
+    x2.r = dot(shBr, vB);
+    x2.g = dot(shBg, vB);
+    x2.b = dot(shBb, vB);
+
+    // Final (5th) quadratic (L2) polynomial
+    float vC = N.x * N.x - N.y * N.y;
+    float3 x3 = shC.rgb * vC;
+
+    return x2 + x3;
+}
+
+
+float3 SampleSH9(float4 SHCoefficients[7], float3 N)
+{
+    float4 shAr = SHCoefficients[0];
+    float4 shAg = SHCoefficients[1];
+    float4 shAb = SHCoefficients[2];
+    float4 shBr = SHCoefficients[3];
+    float4 shBg = SHCoefficients[4];
+    float4 shBb = SHCoefficients[5];
+    float4 shCr = SHCoefficients[6];
+
+    // Linear + constant polynomial terms
+    float3 res = SHEvalLinearL0L1(N, shAr, shAg, shAb);
+
+    // Quadratic polynomials
+    res += SHEvalLinearL2(N, shBr, shBg, shBb, shCr);
+
+    return res;
+}
+
+
+// Samples SH L0, L1 and L2 terms
+half3 SampleSH(half3 normalWS)
+{
+    // LPPV is not supported in Ligthweight Pipeline
+    float4 SHCoefficients[7];
+    SHCoefficients[0] = unity_SHAr;
+    SHCoefficients[1] = unity_SHAg;
+    SHCoefficients[2] = unity_SHAb;
+    SHCoefficients[3] = unity_SHBr;
+    SHCoefficients[4] = unity_SHBg;
+    SHCoefficients[5] = unity_SHBb;
+    SHCoefficients[6] = unity_SHC;
+
+    return max(half3(0, 0, 0), SampleSH9(SHCoefficients, normalWS));
+}
+
+float D_GGXNoPI(float NdotH, float a2)
+{
+    float s = (NdotH * a2 - NdotH) * NdotH + 1.0;
+    return a2/ (s * s);
+}
+#endif // FORWARD_PASS_HLSL
---
+++ Shader/pbr/pbr1/pbr1.shader
@@ -0,0 +1,105 @@
+Shader "Unlit/pbr1"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Metallic("_Metallic",range(0,1)) = 0.5
+        _Smoothness("_Smoothness",range(0,1)) = 0.5
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+            #include "ForwardPass.hlsl"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float3 normal:NORMAL;
+                float4 tangent:TANGENT;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float4 tSpace0:TEXCOORD1;
+                float4 tSpace1:TEXCOORD2;
+                float4 tSpace2:TEXCOORD3;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            float _Smoothness,_Metallic;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = TransformObjectToHClip(v.vertex.xyz);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                float3 n = TransformObjectToWorldNormal(v.normal);
+                float3 worldPos = TransformObjectToWorld(v.vertex);
+                o.tSpace0 = float4(0,0,n.x,worldPos.x);
+                o.tSpace1 = float4(0,0,n.y,worldPos.y);
+                o.tSpace2 = float4(0,0,n.z,worldPos.z);
+
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                float4 mainTex = tex2D(_MainTex, i.uv);
+                float3 albedo = mainTex.xyz;
+                float alpha = mainTex.w;
+
+                float3 n = float3(i.tSpace0.z,i.tSpace1.z,i.tSpace2.z);
+                float3 worldPos = float3(i.tSpace0.w,i.tSpace1.w,i.tSpace2.w);
+                float3 v = GetWorldSpaceViewDir(worldPos);
+                float3 l = GetWorldSpaceLightDir(worldPos);
+
+                float metallic = _Metallic;
+                float smoothness = _Smoothness;
+                float roughness = 1 - smoothness;
+                float a = roughness * roughness;
+                float a2  =a*a;
+
+                float4 col = 0;
+
+                // gi
+                float3 sh = SampleSH(n);
+                float3 giDiff = sh * albedo;
+                col.xyz = giDiff;
+
+                // direct light
+                float3 h = normalize(l+v);
+                float nh = saturate(dot(n,h));
+                float nl = saturate(dot(n,l));
+
+                float specTerm = D_GGXNoPI(nh,a2);
+                float3 specColor = lerp(0.04,albedo,metallic) * specTerm;
+
+                float3 diffColor = albedo;
+                diffColor *= 1 - metallic;
+
+                float3 radiance = _MainLightColor * nl;
+
+                col.xyz += (diffColor + specColor) * radiance;
+                col.w = alpha;
+
+                return col;
+            }
+            ENDHLSL
+        }
+    }
+}
